4483f8ac3da9c21f72cdad7a38b64f7c8d289a61
Guillaume de Saint Etienne <guillaume.desaintetienne@edu.ece.fr>
2025-12-27 00:44:46 +0100

    ajout docker

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'xsiipwhc5v.eu-west-3.awsapprunner.com'
+    ],
+    host: true,
+    port: 8080
+  }
 })
